30b6d214a7e75823c378266980a853bd3db89f9d
Nilyass <47905754+Nilyass@users.noreply.github.com>
2019-05-06 21:06:38 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("Casa BroadCast System",{type: 'STREAMING'})
+   client.user.setActivity("K,codes | +bc",{type: 'STREAMING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
